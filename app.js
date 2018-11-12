413a1d63f8ec673e263d1bd2e6c65473db48d328
Ludwig Muench <muench.ludwig@gmail.com>
2018-11-12 23:37:19 +0100

    change API

--- app.js
+++ app.js
@@ -6,28 +6,32 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/temp1', async (req, res) => {
-  console.log('GET /temp1');
-  res.json({ value: 25.6 });
+app.get('/temp', async (req, res) => {
+  console.log('GET /temp');
+  setTimeout(() => {
+    res.json({ value: 25.6 });
+  }, 0);
 });
 
-app.get('/humid1', async (req, res) => {
-  console.log('GET /humid1');
-  res.json({ value: 65.3 });
+app.get('/humid', async (req, res) => {
+  console.log('GET /humid');
+  setTimeout(() => {
+    res.json({ value: 65.7 });
+  }, 1000)
 });
 
-const light1 = {};
-light1.value = 0;
+const light = {};
+light.value = 0;
 
-app.get('/light1', async (req, res) => {
-  console.log('GET /light1:');
-  res.json(light1);
+app.get('/light', async (req, res) => {
+  console.log('GET /light:');
+  res.json(light);
 });
 
-app.put('/light1', async (req, res) => {
-  console.log('PUT /light1:', req.body);
-  light1.value = req.body.value;
-  res.json(light1);
+app.put('/light', async (req, res) => {
+  console.log('PUT /light:', req.body);
+  light.value = req.body.value;
+  res.json(light);
 });
 
 app.listen(5000, () => {
