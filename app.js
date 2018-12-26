eaeecd540398db1a3a61a485b7069657229c13ce
Ludwig Muench <muench.ludwig@gmail.com>
2018-12-26 16:59:40 +0100

    run both simulated endpoints from one file; remove yarn lockfile

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-app.use(cors());
-app.use(bodyParser.json());
+const app1 = express();
 
-app.get('/temp', async (req, res) => {
+app1.use(cors());
+app1.use(bodyParser.json());
+
+app1.get('/temp', async (req, res) => {
   console.log('GET /temp');
   setTimeout(() => {
     res.json({ value: 25.6 });
   }, 0);
 });
 
-app.get('/humid', async (req, res) => {
+app1.get('/humid', async (req, res) => {
   console.log('GET /humid');
   // delay response by 100 ms
   setTimeout(() => {
@@ -21,20 +22,46 @@ app.get('/humid', async (req, res) => {
   }, 100)
 });
 
-const light = {};
-light.value = 0;
+const light1 = {};
+light1.value = 0;
 
-app.get('/light', async (req, res) => {
+app1.get('/light', async (req, res) => {
   console.log('GET /light:');
-  res.json(light);
+  res.json(light1);
 });
 
-app.put('/light', async (req, res) => {
+app1.put('/light', async (req, res) => {
   console.log('PUT /light:', req.body);
-  light.value = req.body.value;
-  res.json(light);
+  light1.value = req.body.value;
+  res.json(light1);
 });
 
-app.listen(5000, () => {
-  console.log('Simulated endpoint running on port 5000');
+app1.listen(5000, () => {
+  console.log('First simulated endpoint running on port 5000');
+})
+
+// --------------------
+
+const app2 = express();
+
+app2.use(cors());
+app2.use(bodyParser.json());
+
+const light2 = {};
+light2.value = 0;
+
+app2.get('/light', async (req, res) => {
+  console.log('GET /light:');
+  res.json(light2);
+});
+
+app2.put('/light', async (req, res) => {
+  console.log('PUT /light:', req.body);
+  light2.value = req.body.value;
+  res.json(light2);
+});
+
+
+app2.listen(5001, () => {
+  console.log('Second simulated endpoint running on port 5001');
 })
